3edb512b2b759c6e0e8235f98f02801a2a159b5f
GingerKittenOwO <110766733+GingerKittenOwO@users.noreply.github.com>
2025-09-17 22:52:59 +0300

    Added .class to .gitignore and completed the first exercise of the second lab

---
+++ src/ru/mirea/lab2/TestAuthor.java
@@ -0,0 +1,20 @@
+package ru.mirea.lab2;
+
+import java.util.Scanner;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        String email;
+        Author ath = new Author("Ivan","vasilev.i.i1@edu.mirea.ru",'m');
+        System.out.println(ath);
+        System.out.println(ath.getEmail());
+        System.out.println(ath.getGender());
+        System.out.println(ath.getName());
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Введите новый email: ");
+        email = sc.nextLine();
+        ath.setEmail(email);
+        System.out.println(ath.getEmail());
+    }
+
+}
