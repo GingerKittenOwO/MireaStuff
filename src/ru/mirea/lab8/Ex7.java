23ad0983fa88d4a7569a007a4bdbf36b177ac3bc
GingerKittenOwO <110766733+GingerKittenOwO@users.noreply.github.com>
2025-10-09 21:52:24 +0300

    Finished ex7 lab 8 (recursion)

--- src/ru/mirea/lab8/Ex7.java
+++ src/ru/mirea/lab8/Ex7.java
@@ -5,17 +5,18 @@ import java.util.Scanner;
 import static java.lang.Math.sqrt;
 
 public class Ex7 {
-    public static String simple_num(int n, int del){
-        if (del>sqrt(n)+1) {System.out.println(n); return"";}
-        else if (n%del==0) System.out.println(del);
-
-        return simple_num(n,del+1);
+    public static String multipliers(int n, int del){
+        if (n==1) { return"";}
+        else if (n%del==0){
+            System.out.println(del);
+            return multipliers(n/del,del);}
+        else return multipliers(n,del+1);
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        simple_num(a,1);
+        multipliers(a,2);
     }
 }
