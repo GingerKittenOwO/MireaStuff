49cd0023582484453674d20b89e02485c7556d3c
RusskoeLotoBilet <110766733+GingerKittenOwO@users.noreply.github.com>
2025-10-25 19:31:58 +0300

    small changes

--- src/ru/mirea/lab9/StudentTest.java
+++ src/ru/mirea/lab9/StudentTest.java
@@ -1,11 +1,27 @@
 package ru.mirea.lab9;
+import java.util.Arrays;
+import java.util.List;
 
 public class StudentTest {
     public static void main(String[] args) {
-        Student[] arr = new Student[]{new Student(4),new Student(3),new Student(6),new Student(5)};
+        Student[] arr = new Student[]{new Student(4,"Ivan",34),
+                new Student(3,"Vlad",20),
+                new Student(6,"Egor",86),
+                new Student(5,"Misha",78)};
 
         for (Student st : arr) System.out.println(st);
         Student.insertSort(arr);
         for (Student st : arr) System.out.println(st);
+
+
+        System.out.println("\nSorting by GPA:");
+        SortingStudentsByGPA srtGPA = new SortingStudentsByGPA();
+        List<Student> list = Arrays.asList(arr);
+        srtGPA.sortByGPA(list);
+        for (Student student : list){
+            System.out.println(student);
+        }
+
+
     }
 }
